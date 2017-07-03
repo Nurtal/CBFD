30501b13ed7d0b407fae907a4e3cde3a01dafcef
Nurtal <nathan.foulquier.pro@gmail.com>
2017-07-03 11:25:48 +0200

    update

--- pca.R
+++ pca.R
@@ -24,6 +24,12 @@ data <- read.csv(data_file_name, stringsAsFactors=TRUE, sep=",")
 ##------------##
 ## Imputation ##
 ##------------##
+library(mice)
+md.pattern(data)
+
+library(VIM)
+aggr_plot <- aggr(data, col=c('navyblue','red'), numbers=TRUE, sortVars=TRUE, labels=names(data), cex.axis=.7, gap=3, ylab=c("Histogram of missing data","Pattern"))
+
 
 ##-------------------------##
 ## Scaling data before PCA ##
